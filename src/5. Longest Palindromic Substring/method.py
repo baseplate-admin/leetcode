7dd2c5ca1ab92aab88c673f778aa56b09dcbe453
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-21 23:54:24 +0600

    Create method.py

---
+++ src/5. Longest Palindromic Substring/method.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        for i in range(len(s)):
+            left = right = i
+            
+
+
+Solution().longestPalindrome("babad")
